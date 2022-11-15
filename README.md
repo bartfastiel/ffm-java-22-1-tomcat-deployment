7794db5cbdc4096331f91f478584dec6545183b2
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2022-11-15 10:27:07 +0100

    Setup project

---
+++ README.md
@@ -0,0 +1,3 @@
+ec2-3-73-50-34.eu-central-1.compute.amazonaws.com
+admin
+uMXFqJQ5TyuiRXhc
---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import axios from "axios";
+
+function App() {
+
+    const [message, setMessage] = useState<string>();
+
+    useEffect(() => {
+        axios.get("/api/cats")
+            .then(response => response.data)
+            .then(setMessage)
+    }, [])
+
+    return <>
+        {message}
+    </>;
+}
+
+export default App;
